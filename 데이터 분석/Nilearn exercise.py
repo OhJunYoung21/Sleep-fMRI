8b2bb73989e87f85bb84cb0db211e0d66aedac0b
JUNYONG <dhwnsdyd21@gmail.com>
2024-05-02 14:42:39 +0900

    Create Nilearn exercise.py

---
+++ 데이터 분석/Nilearn exercise.py
@@ -0,0 +1,6 @@
+~~~python3
+import nilearn
+from nilearn import plotting
+file_path = "/Users/ojun-yong/Desktop/bids_ex/fmriprep/sub-01/ses-1/anat/sub-01_ses-1_acq-Sagittal3DT1GRE_rec-BRAINMRINONCONTRASTDIFFUSION_space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz"
+plotting.plot_img(file_path)
+~~~
