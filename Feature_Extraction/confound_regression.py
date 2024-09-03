eb59c36e9952168c55689a43220d92f755f790d5
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-09-03 17:17:07 +0900

    confound_regression commit

--- Feature_Extraction/confound_regression.py
+++ Feature_Extraction/confound_regression.py
@@ -15,9 +15,21 @@ root_dir = '/Users/oj/Desktop/Yoo_Lab/post_fMRI/post_prep_RBD'
 fMRI_img = glob.glob(os.path.join(root_dir, 'sub-*', 'func',
                                   'sub-*_task-BRAINMRINONCONTRASTDIFFUSION_acq-AxialfMRIrest_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'))
 
-confounds = glob.glob(os.path.join(root_dir, 'sub-[0-9][0-9]', 'func',
-                                   'sub-[0-9][0-9]_task-BRAINMRINONCONTRASTDIFFUSION_acq-AxialfMRIrest_desc-confounds_timeseries.tsv'))
+raw_confounds = glob.glob(os.path.join(root_dir, 'sub-[0-9][0-9]', 'func',
+                                       'sub-[0-9][0-9]_task-BRAINMRINONCONTRASTDIFFUSION_acq-AxialfMRIrest_desc-confounds_timeseries.tsv'))
 
-temp_img = image.index_img(fMRI_img[0],10)
-plotting.plot_img(temp_img)
-plotting.show()
+for index in range(len(fMRI_img)):
+    # 혼란변수 파일 가져오기(pandas 사용)
+    confounds = pd.read_csv(raw_confounds[index], sep='\t')
+    # fMRI_img 가져오기
+    fmri_img = fMRI_img[index]
+
+    confounds_of_interest = confounds[
+        ['trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z',
+         'global_signal', 'white_matter', 'csf']
+    ]
+
+    cleaned_img = clean_img(fmri_img, confound=confounds_of_interest)
+
+
+    cleaned_img.to_filename(f"{root_dir}/sub-{index}_confounds_regressed.nii.gz")
