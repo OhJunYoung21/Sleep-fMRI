c34be6a67abcefaa07e8fb9b8b87942ab868d56c
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-09-05 15:31:59 +0900

    confound_regression commit

--- Feature_Extraction/confound_regression.py
+++ Feature_Extraction/confound_regression.py
@@ -7,6 +7,7 @@ from nilearn.image import get_data
 from nilearn.image import clean_img
 from nilearn.interfaces.fmriprep import load_confounds
 from nilearn import plotting
+import re
 
 root_dir = '/Users/oj/Desktop/Yoo_Lab/post_fMRI/post_prep_RBD'
 
@@ -22,23 +23,22 @@ fMRI_img = sorted(fMRI_img)
 
 raw_confounds = sorted(raw_confounds)
 
-print(raw_confounds)
+## print(raw_confounds)
 
-'''
+
 for index in range(len(fMRI_img)):
-    # 혼란변수 파일 가져오기(pandas 사용)
-    confounds = pd.read_csv(raw_confounds[index], sep='\t')
-    # fMRI_img 가져오기
-    fmri_img = fMRI_img[index]
+    # fMRI_image와 confounds 업로드
 
-    print(fMRI_img[index])
+    fmri_img = fMRI_img[index]
+    confounds = pd.read_csv(raw_confounds[index], sep='\t')
 
     confounds_of_interest = confounds[
         ['trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z',
          'global_signal', 'white_matter', 'csf']
     ]
+    # 정규표현식을 사용해서 subject_number를 얻어온다. index+1로 얻어오는 경우, subject가 7,9인 경우 9를 8로 적어버리는 오류가 발생한다.
+    subject_number = re.search(r'sub-(\d+)', fMRI_img[index]).group(1)
 
-    ### cleaned_img = clean_img(fmri_img, confound=confounds_of_interest)
+    cleaned_image = clean_img(fmri_img, confounds=confounds_of_interest)
 
-    ### cleaned_img.to_filename(f"{root_dir}/sub-{index + 1}_confounds_regressed.nii.gz")
-'''
+    cleaned_image.to_filename(f"{root_dir}/sub-{subject_number}_confounds_regressed.nii.gz")
