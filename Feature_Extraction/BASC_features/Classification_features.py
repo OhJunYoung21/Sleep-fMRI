a3932263067359df971b16a36c654d43fee9e008
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-09-09 11:31:39 +0900

    BASC_extraction commit

--- Feature_Extraction/BASC_features/Classification_features.py
+++ Feature_Extraction/BASC_features/Classification_features.py
@@ -5,17 +5,25 @@ import os
 from nilearn import image
 from nilearn import plotting
 
-basc_atlas = datasets.fetch_atlas_basc_multiscale_2015()
-basc_atlas_name = basc_atlas["scale036"]
 
-file_path = '/Users/oj/Desktop/Yoo_Lab/post_fMRI/confounds_regressed_RBD/sub-01_confounds_regressed.nii.gz'
-data = image.load_img(file_path)
+def FC_extraction(file_path):
+    basc_atlas = datasets.fetch_atlas_basc_multiscale_2015()
+    basc_atlas_name = basc_atlas["scale036"]
 
-masker = NiftiLabelsMasker(labels_img=basc_atlas_name, standardize=True)
-time_series = masker.fit_transform(data)
+    data = image.load_img(file_path)
 
-correlation_measure = ConnectivityMeasure(kind='correlation')
-correlation_matrix = correlation_measure.fit_transform([time_series])[0]
+    masker = NiftiLabelsMasker(labels_img=basc_atlas_name, standardize=True)
+    time_series = masker.fit_transform(data)
 
-plotting.plot_matrix(correlation_matrix)
-plotting.show()
+    correlation_measure = ConnectivityMeasure(kind='correlation')
+    correlation_matrix = correlation_measure.fit_transform([time_series])[0]
+
+    return correlation_matrix
+
+
+def ALFF_extraction(file_path):
+    return
+
+
+def Reho_extraction(file_path):
+    return
