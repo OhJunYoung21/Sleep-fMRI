5feec2ec9db7de0c0bc03549f922fcfbe45fd3a8
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-09-30 12:42:15 +0900

    Model_train commit

--- Models/SVM.py
+++ Models/SVM.py
@@ -2,4 +2,10 @@ import pandas as pd
 
 file = pd.read_csv('/Users/oj/Desktop/Yoo_Lab/Classification_Features/Shen/Shen_features.csv')
 
-print(file.head())
+'''
+TO DO LIST
+
+- Train SVM model with FC
+- FC data manipulation
+- ALFF data check 
+'''
