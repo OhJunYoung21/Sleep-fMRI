2cf34531431f5d31d1270dd232b7b4e7bebd000d
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-10-02 15:22:50 +0900

    Model_data commit

--- Models/Data.py
+++ Models/Data.py
@@ -8,4 +8,14 @@ import numpy as np
 
 data = pd.DataFrame(pd.read_csv('/Users/oj/Desktop/Yoo_Lab/Classification_Features/Shen/Shen_features.csv'))
 
+data.loc[:, 'FC'] = data['FC'].apply(ast.literal_eval)
 
+'''
+X = data['FC']
+y = data['STATUS']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+Model = SVC(kernel='linear')
+Model.fit(X_train, y_train)
+'''
