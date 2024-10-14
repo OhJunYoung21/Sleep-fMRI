32751b2b388663ed357920ac621c99680d2b91fe
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-10-14 16:27:09 +0900

    init commit

--- Visualization/Visualization.py
+++ Visualization/Visualization.py
@@ -10,21 +10,39 @@ from nilearn.image import threshold_img
 from Feature_Extraction.Shen_features.Classification_feature import FC_extraction, file_path, atlas_path
 from Feature_Extraction.Shen_features.Classification_feature import region_alff_average
 
-
-alff_path = '/Users/oj/Desktop/Yoo_Lab/CPAC/RBD/sub-09/results/alff.nii.gz'
+reho_path_afni = '/Users/oj/Desktop/Yoo_Lab/post_fMRI/confounds_regressed_RBD/reho/reho_01.nii.gz'
+reho_path_CPAC = '/Users/oj/Desktop/Yoo_Lab/CPAC/RBD/sub-01/results/ReHo.nii.gz'
+alff_path = '/Users/oj/Desktop/Yoo_Lab/CPAC/RBD/sub-01/results/alff.nii.gz'
+falff_path = '/Users/oj/Desktop/Yoo_Lab/CPAC/RBD/sub-01/results/falff.nii.gz'
 
 alff_img = image.load_img(alff_path)
+falff_img = image.load_img(falff_path)
+reho_img_CPAC = image.load_img(reho_path_CPAC)
+reho_img_afni = image.load_img(reho_path_afni)
 
 shen_atlas = input_data.NiftiLabelsMasker(labels_img=atlas_path, standardize=True, strategy='mean',
                                           resampling_target="labels")
+'''
+alff_data = shen_atlas.fit_transform(alff_img)
+falff_data = shen_atlas.fit_transform(falff_img)
+reho_data = shen_atlas.fit_transform(reho_img)
 
-data = shen_atlas.fit_transform(alff_img)
+alff_img_masked = shen_atlas.inverse_transform(alff_data)
+falff_img_masked = shen_atlas.inverse_transform(falff_data)
+reho_img_masked = shen_atlas.inverse_transform(reho_data)
 
+plotting.plot_stat_map(reho_img_masked, title="Shen_ReHo")
+plotting.show()
+'''
 
-alff_img_masked = shen_atlas.inverse_transform(data)
+reho_data_afni = shen_atlas.fit_transform(reho_img_afni)
+reho_img_afni = shen_atlas.inverse_transform(reho_data_afni)
 
-plotting.plot_stat_map(alff_img_masked, title="Shen_ALFF")
+plotting.plot_stat_map(reho_img_afni, title="Shen_ReHo_AFNI")
 plotting.show()
 
+reho_data_CPAC = shen_atlas.fit_transform(reho_img_CPAC)
+reho_img_CPAC = shen_atlas.inverse_transform(reho_data_CPAC)
 
-
+plotting.plot_stat_map(reho_img_CPAC, title="Shen_ReHo_CPAC")
+plotting.show()
