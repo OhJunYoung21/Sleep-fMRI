9fa64fae8804692d90dbcf4ca0433f2f8d61f722
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-10-22 17:17:28 +0900

    upgraded_features using RandomForestClassifier commit

--- Models/SVM.py
+++ Models/SVM.py
@@ -6,6 +6,13 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 import numpy as np
 from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+
+upgraded_features = pd.DataFrame()
+
+upgraded_features['up_ALFF'] = None
+upgraded_features['STATUS'] = None
+upgraded_features['ALFF'] = None
 
 shen_features = pd.read_csv('/Users/oj/Desktop/Yoo_Lab/Classification_Features/Shen/Shen_features_final.csv',
                             converters={'ALFF': ast.literal_eval,
@@ -25,6 +32,27 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 X_train = np.array(X_train.tolist())
 X_test = np.array(X_test.tolist())
 
+X_pre_train, X_pre_test, y_pre_train, y_pre_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier()
+model.fit(X_pre_train, y_pre_train)
+importance = model.feature_importances_
+
+values_array = np.array(importance.tolist())
+
+threshold = np.percentile(values_array, 90)
+
+top_10_percent_indices = np.where(values_array >= threshold)[0]
+
+upgraded_features['ALFF'] = X_pre_train.tolist()
+upgraded_features['up_ALFF'] = upgraded_features['ALFF'].apply(lambda x: [x[i] for i in top_10_percent_indices])
+
+print(upgraded_features.head())
+
+'''
+print(upgraded_features)
+
+
 clf = svm.SVC(kernel='linear')
 clf.fit(X_train, y_train)
 
@@ -36,7 +64,7 @@ result_matrix = confusion_matrix(y_test, y_pred)
 
 print(result_matrix,len(y_pred))
 
-'''
+
 target = shen_features['FC'][1]
 
 plotting.plot_matrix(target,
--- Visualization/generate_mean_img.py
+++ Visualization/generate_mean_img.py
@@ -4,20 +4,24 @@ from nilearn.image import mean_img
 from nilearn import image, input_data
 from nilearn import plotting
 from Feature_Extraction.Shen_features.Classification_feature import atlas_path
+from scipy import stats
 
 reho_sub_1 = '/Users/oj/Desktop/Yoo_Lab/CPAC/RBD/sub-01/results/ReHo.nii.gz'
 reho_sub_2 = '/Users/oj/Desktop/Yoo_Lab/CPAC/RBD/sub-02/results/ReHo.nii.gz'
 
-mean_reho = mean_img(imgs=[reho_sub_1, reho_sub_2], n_jobs=2)
-
-result = image.load_img(mean_reho)
+result_1 = image.load_img(reho_sub_1)
+result_2 = image.load_img(reho_sub_2)
 
 shen_atlas = input_data.NiftiLabelsMasker(labels_img=atlas_path, standardize=True, strategy='mean',
                                           resampling_target="labels")
 
-reho_rbd_data = shen_atlas.fit_transform(result)
+reho_rbd_1 = shen_atlas.fit_transform(result_1)
+reho_rbd_2 = shen_atlas.fit_transform(result_2)
 
-rbd_img_masked = shen_atlas.inverse_transform(reho_rbd_data)
+rbd_img_1 = shen_atlas.inverse_transform(reho_rbd_1)
+rbd_img_2 = shen_atlas.inverse_transform(reho_rbd_2)
 
-plotting.plot_stat_map(rbd_img_masked)
-plotting.show()
+t_stat, p_value = stats.ttest_ind(rbd_img_1, rbd_img_2)
+
+print(f"T-statistic: {t_stat}")
+print(f"p-value: {p_value}")
