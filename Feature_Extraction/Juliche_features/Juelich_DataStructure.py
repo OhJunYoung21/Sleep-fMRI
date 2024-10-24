ee33dd341e0b602678a911ddcf3bb3de57cf0423
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-10-24 16:37:26 +0900

    Juelich_features add comma commit

--- Feature_Extraction/Juliche_features/Juelich_DataStructure.py
+++ Feature_Extraction/Juliche_features/Juelich_DataStructure.py
@@ -4,21 +4,19 @@ import os
 import numpy as np
 import glob
 from Feature_Extraction.Shen_features.Classification_feature import calculate_Bandpass
-from Feature_Extraction.Shen_features.Classification_feature import calculate_3dReHo
-from Feature_Extraction.Shen_features.Classification_feature import region_reho_average
-from Feature_Extraction.Shen_features.Classification_feature import atlas_path, FC_extraction
-from Feature_Extraction.Shen_features.Classification_feature import region_alff_average
 from typing import List
 from CPAC import alff
 from Comparison_features.rsfmri import static_measures
+from Feature_Extraction.Juliche_features.Juelich_features import FC_extraction, juelich_alff_average, \
+    juelich_reho_average
 
-shen_data = pd.DataFrame(index=None)
+juelich_data = pd.DataFrame(index=None)
 
-shen_data['FC'] = None
-shen_data['ALFF'] = None
-shen_data['REHO'] = None
-shen_data['fALFF'] = None
-shen_data['STATUS'] = None
+juelich_data['FC'] = None
+juelich_data['ALFF'] = None
+juelich_data['REHO'] = None
+juelich_data['fALFF'] = None
+juelich_data['STATUS'] = None
 
 ReHo_RBD = []
 FC_RBD = []
@@ -54,8 +52,8 @@ mask_path_hc = '/Users/oj/Desktop/Yoo_Lab/mask_hc'
 
 ## 데이터프레임안의 요소들을 전부 지우는 함수이다. 혹시나 데이터프레임안의 데이터가 꼬이는 경우에 빠른 초기화를 위해 제작하였다.
 def delete():
-    shen_data.iloc[:, :] = None
-    return shen_data
+    juelich_data.iloc[:, :] = None
+    return juelich_data
 
 
 ### reho를 계산해서 reho 디렉토리 안에 넣어주는 코드
@@ -66,7 +64,7 @@ def input_fc(files_path: str, data: List):
     files = sorted(files)
 
     for file in files:
-        connectivity = FC_extraction(file, atlas_path)
+        connectivity = FC_extraction(file)
 
         connectivity = connectivity.tolist()
 
@@ -79,24 +77,6 @@ def input_fc(files_path: str, data: List):
 
     return
 
-
-def input_reho(files_path: str):
-    files = glob.glob(os.path.join(files_path, 'sub-*_confounds_regressed.nii.gz'))
-
-    files = sorted(files)
-
-    for file in files:
-        match = re.search(r'sub-(.*)_confounds_regressed.nii.gz', file)
-
-        if match:
-            extracted_part = match.group(1)
-
-        calculate_3dReHo(file, extracted_part, os.path.join(files_path + '/reho'))
-
-    return
-
-
-### input_alff()는 alff파일을 만들어서 로컬에 저장하는 함수입니다.
 
 def input_features(files_path: str, mask_path: str, status: str):
     fmri_files = glob.glob(os.path.join(files_path, 'sub-*_confounds_regressed.nii.gz'))
@@ -119,38 +99,38 @@ def input_features(files_path: str, mask_path: str, status: str):
     return
 
 
-def make_reho_shen(file_path: str, data: List):
+def make_reho_juelich(file_path: str, data: List):
     reho_path = glob.glob(os.path.join(file_path, 'sub-*/results/ReHo.nii.gz'))
 
     for file in reho_path:
         ##Classification_feature에서 불러온 atlas_path를 매개변수로 넣어준다.
-        shen_reho = region_reho_average(file, atlas_path)
-        data.append(shen_reho)
+        juelich_reho = juelich_reho_average(file)
+        data.append(juelich_reho)
     return
 
 
 ### 로컬의 alff폴더에서 파일을 읽어온 후, shen_atlas를 적용하고 ALFF 리스트에 넣어준다.
-def make_alff_shen(file_path: str, data: List):
+def make_alff_juelich(file_path: str, data: List):
     alff_path = glob.glob(os.path.join(file_path, 'sub-*/results/alff.nii.gz'))
 
     alff_path = sorted(alff_path)
 
     for file in alff_path:
         ##Classification_feature에서 불러온 atlas_path를 매개변수로 넣어준다.
-        shen_alff = region_alff_average(file, atlas_path)
-        data.append(shen_alff)
+        juelich_alff = juelich_alff_average(file)
+        data.append(juelich_alff)
     return
 
 
-def make_falff_shen(file_path: str, data: List):
-    alff_path = glob.glob(os.path.join(file_path, 'sub-*/results/falff.nii.gz'))
+def make_falff_juelich(file_path: str, data: List):
+    falff_path = glob.glob(os.path.join(file_path, 'sub-*/results/falff.nii.gz'))
 
-    alff_path = sorted(alff_path)
+    falff_path = sorted(falff_path)
 
-    for file in alff_path:
+    for file in falff_path:
         ##Classification_feature에서 불러온 atlas_path를 매개변수로 넣어준다.
-        shen_alff = region_alff_average(file, atlas_path)
-        data.append(shen_alff)
+        juelich_falff = juelich_alff_average(file)
+        data.append(juelich_falff)
     return
 
 
@@ -159,25 +139,25 @@ input_features(root_rbd_dir, mask_path_rbd, "RBD")
 input_features(root_hc_dir, mask_path_hc, "HC")
 '''
 
-make_reho_shen(CPAC_hc, ReHo_HC)
-make_alff_shen(CPAC_hc, ALFF_HC)
-make_falff_shen(CPAC_hc, fALFF_HC)
+make_reho_juelich(CPAC_hc, ReHo_HC)
+make_alff_juelich(CPAC_hc, ALFF_HC)
+make_falff_juelich(CPAC_hc, fALFF_HC)
 input_fc(root_hc_dir, FC_HC)
 
-make_reho_shen(CPAC_rbd, ReHo_RBD)
-make_alff_shen(CPAC_rbd, ALFF_RBD)
-make_falff_shen(CPAC_rbd, fALFF_RBD)
+make_reho_juelich(CPAC_rbd, ReHo_RBD)
+make_alff_juelich(CPAC_rbd, ALFF_RBD)
+make_falff_juelich(CPAC_rbd, fALFF_RBD)
 input_fc(root_rbd_dir, FC_RBD)
 
 len_hc = len(ReHo_HC)
 len_rbd = len(ReHo_RBD)
 
 for j in range(len_hc):
-    shen_data.loc[j] = [FC_HC[j], ALFF_HC[j], ReHo_HC[j], fALFF_HC[j], 0]
+    juelich_data.loc[j] = [FC_HC[j], ALFF_HC[j], ReHo_HC[j], fALFF_HC[j], 0]
 
 for k in range(len_rbd):
-    shen_data.loc[len_hc + k] = [FC_RBD[k], ALFF_RBD[k], ReHo_RBD[j], fALFF_RBD[j], 1]
+    juelich_data.loc[len_hc + k] = [FC_RBD[k], ALFF_RBD[k], ReHo_RBD[j], fALFF_RBD[j], 1]
 
-shen_data_path = os.path.join(feature_path, 'Shen/Shen_features_ex')
+juelich_data_path = os.path.join(feature_path, 'Juelich/Juelich_features.csv')
 
-shen_data.to_csv(shen_data_path, index=False)
+juelich_data.to_csv(juelich_data_path, index=False)
--- Models/SVM.py
+++ Models/SVM.py
@@ -14,45 +14,43 @@ upgraded_features['up_ALFF'] = None
 upgraded_features['STATUS'] = None
 upgraded_features['ALFF'] = None
 
-shen_features = pd.read_csv('/Users/oj/Desktop/Yoo_Lab/Classification_Features/Shen/Shen_features_final.csv',
-                            converters={'ALFF': ast.literal_eval,
-                                        'fALFF': ast.literal_eval,
-                                        'REHO': ast.literal_eval,
-                                        'STATUS': ast.literal_eval,
-                                        'FC': ast.literal_eval}
-                            )
-shen_features['ALFF'] = shen_features['ALFF'].apply(
+juelich_features = pd.read_csv('/Users/oj/Desktop/Yoo_Lab/Classification_Features/Juelich/Juelich_features.csv',
+                               converters={'ALFF': ast.literal_eval,
+                                           'fALFF': ast.literal_eval,
+                                           'REHO': ast.literal_eval,
+                                           'STATUS': ast.literal_eval,
+                                           'FC': ast.literal_eval}
+                               )
+
+alff_to_process = 'ALFF'  # 처리할 컬럼 이름
+juelich_features[alff_to_process] = juelich_features[alff_to_process].str.split().str.join(',').flatten()
+reho_to_process = 'REHO'  # 처리할 컬럼 이름
+juelich_features[reho_to_process] = juelich_features[reho_to_process].str.split().str.join(',').flatten()
+falff_to_process = 'fALFF'  # 처리할 컬럼 이름
+juelich_features[falff_to_process] = juelich_features[falff_to_process].str.split().str.join(',').flatten()
+juelich_features.to_csv('/Users/oj/Desktop/Yoo_Lab/Classification_Features/Juelich/Juelich_features_final.csv', index=False)
+
+
+'''
+juelich_features['ALFF'] = juelich_features['ALFF'].apply(
     lambda x: np.array(x).flatten())
 
-X = np.array(shen_features['ALFF'])
-y = shen_features['STATUS']
+X = np.array(juelich_features['ALFF'])
+y = juelich_features['STATUS']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 X_train = np.array(X_train.tolist())
 X_test = np.array(X_test.tolist())
 
-X_pre_train, X_pre_test, y_pre_train, y_pre_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-
-model = RandomForestClassifier()
-model.fit(X_pre_train, y_pre_train)
-importance = model.feature_importances_
-
-values_array = np.array(importance.tolist())
-
-threshold = np.percentile(values_array, 90)
-
-top_10_percent_indices = np.where(values_array >= threshold)[0]
-
-'''
 clf = svm.SVC(kernel='linear')
-clf.fit(X_sample_train, y_sample_train)
+clf.fit(X_train, y_train)
 
-y_sample_pred = clf.predict(X_sample_test)
+y_pred = clf.predict(X_test)
 
-acc = accuracy_score(y_sample_test, y_sample_pred)
+acc = accuracy_score(y_test, y_pred)
 
-result_matrix = confusion_matrix(y_sample_test, y_sample_pred)
+result_matrix = confusion_matrix(y_test, y_pred)
 
 print(acc)
 '''
