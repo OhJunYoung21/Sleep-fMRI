3a1ad1a5093a94d88727bd90c2cd78286f49f97f
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-11-19 16:34:33 +0900

    훈련데이터 내에서 서로 다른 region들 추출 후 모델링(SVM) commit

--- Models/SVM_feature_selcetion.py
+++ Models/SVM_feature_selcetion.py
@@ -12,10 +12,37 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from sklearn.model_selection import KFold, cross_val_score
-
+from Visualization.T_test import check_normality, student_t_test, welch_t_test, mann_whitney_test, check_variance
 
 schaefer_pkl = pd.read_pickle('../schaefer_data_pkl')
 
+
+def avoid_duplication(nested_list):
+    unique_elements = set()
+    for sublist in nested_list:
+        unique_elements.update(sublist)
+
+    # 집합을 다시 리스트로 변환
+    result = list(unique_elements)
+
+    return result
+
+
+def statistic(rbd_data, hc_data):
+    mann_whitneyu, welch, student = check_variance(np.array(rbd_data.tolist()),
+                                                   np.array(hc_data.tolist()))
+
+    student_test = student_t_test(student, np.array(rbd_data.tolist()),
+                                  np.array(hc_data.tolist()))
+    welch_test = welch_t_test(welch, np.array(rbd_data.tolist()),
+                              np.array(hc_data.tolist()))
+    mann_test = mann_whitney_test(mann_whitneyu, np.array(rbd_data.tolist()),
+                                  np.array(hc_data.tolist()))
+
+    return np.unique(mann_test + student_test + welch_test).tolist()
+
+
+feature_difference = []
 for i in range(20):
     status_1_data = schaefer_pkl[schaefer_pkl['STATUS'] == 1]
     status_0_data = schaefer_pkl[schaefer_pkl['STATUS'] == 0]
@@ -32,7 +59,21 @@ for i in range(20):
     train_data = pd.concat([train_data_1, train_data_0])
     test_data = pd.concat([test_data_1, test_data_0])
 
-    rbd_data = train_data[train_data['STATUS'] == 1]
-    hc_data = train_data[train_data['STATUS'] == 0]
+    rbd_data = train_data['REHO'][train_data['STATUS'] == 1]
+    hc_data = train_data['REHO'][train_data['STATUS'] == 0]
 
+    result_list = statistic(rbd_data, hc_data)
 
+    feature_difference.append(result_list)
+
+    result = avoid_duplication(feature_difference)
+
+    train_data['REHO'] = train_data['REHO'].apply(lambda x: [x[i] for i in result])
+    test_data['REHO'] = test_data['REHO'].apply(lambda x: [x[i] for i in result])
+
+    model = svm.SVC(kernel='linear', C=1)
+    model.fit(np.array(train_data['REHO'].tolist()), train_data['STATUS'])
+
+    accuracy = model.score(np.array(test_data['REHO'].tolist()), test_data['STATUS'])
+
+    print(f"{i + 1}th accuracy : {accuracy:.2f}")
--- Visualization/T_test.py
+++ Visualization/T_test.py
@@ -21,22 +21,23 @@ Schaefer_atlas = input_data.NiftiLabelsMasker(labels_img=atlas_filename, standar
                                               resampling_target="labels")
 
 falff_rbd_imgs = glob.glob(os.path.join('/Users/oj/Desktop/Yoo_Lab/CPAC_features/RBD/ALFF', 'reho_*.nii.gz'))
-falff_rbd = []
+feature_list = []
 falff_hc_imgs = glob.glob(os.path.join('/Users/oj/Desktop/Yoo_Lab/CPAC_features/HC/ALFF', 'reho_*.nii.gz'))
 falff_hc = []
 
 for k in falff_rbd_imgs:
     img = image.load_img(k)
     rbd = Schaefer_atlas.fit_transform(img)
-    falff_rbd.append(rbd)
+    feature_list.append(rbd)
 
 for k in falff_hc_imgs:
     img = image.load_img(k)
     hc = Schaefer_atlas.fit_transform(img)
     falff_hc.append(hc)
 
-falff_rbd = np.array([item[0] for item in falff_rbd])
+feature_list = np.array([item[0] for item in feature_list])
 falff_hc = np.array([item[0] for item in falff_hc])
+
 
 # 정규분포를 따르는 region과 그렇지 않은 region의 노드를 알려준다.
 
@@ -56,14 +57,14 @@ def check_normality(features):
     return mann_whitneyu, t_test
 
 
-def check_variance():
-    mann_whitneyu, t_test = check_normality(falff_rbd)
+def check_variance(rbd_list, hc_list):
+    mann_whitneyu, t_test = check_normality(rbd_list)
 
     welch = []
     student = []
 
     for j in t_test:
-        t_stats, p_val = levene(falff_rbd[:, j], falff_hc[:, j])
+        t_stats, p_val = levene(rbd_list[:, j], hc_list[:, j])
 
         # 두그룹의 분산이 동일하지 않은 경우.
         if p_val < 0.05:
@@ -73,14 +74,14 @@ def check_variance():
     return mann_whitneyu, welch, student
 
 
-mann_whitneyu, welch, student = check_variance()
+### mann_whitneyu, welch, student = check_variance()
 
 
-def welch_t_test(welch_list):
+def welch_t_test(welch_list, rbd_data, hc_data):
     result_welch = []
 
     for j in welch_list:
-        t_stats, p_val = ttest_ind(falff_rbd[:, j], falff_hc[:, j], equal_var=False)
+        t_stats, p_val = ttest_ind(rbd_data[:, j], hc_data[:, j], equal_var=False)
 
         if p_val < 0.05:
             result_welch.append(j)
@@ -89,11 +90,11 @@ def welch_t_test(welch_list):
     return result_welch
 
 
-def student_t_test(student_list):
+def student_t_test(student_list, rbd_data, hc_data):
     result_student = []
 
     for j in student_list:
-        t_stats, p_val = ttest_ind(falff_rbd[:, j], falff_hc[:, j], equal_var=True)
+        t_stats, p_val = ttest_ind(rbd_data[:, j], hc_data[:, j], equal_var=True)
 
         if p_val < 0.05:
             result_student.append(j)
@@ -102,11 +103,11 @@ def student_t_test(student_list):
     return result_student
 
 
-def mann_whitney_test(mann_list):
+def mann_whitney_test(mann_list, rbd_data, hc_data):
     result_mann = []
 
     for j in mann_list:
-        u_stats, p_val = mannwhitneyu(falff_rbd[:, j], falff_hc[:, j], alternative='two-sided')
+        u_stats, p_val = mannwhitneyu(rbd_data[:, j], hc_data[:, j], alternative='two-sided')
 
         if p_val < 0.05:
             result_mann.append(j)
@@ -115,6 +116,7 @@ def mann_whitney_test(mann_list):
     return result_mann
 
 
+'''
 student_test = student_t_test(student)
 welch_test = welch_t_test(welch)
 mann_test = mann_whitney_test(mann_whitneyu)
@@ -127,4 +129,4 @@ reho_Schaefer_data = pd.DataFrame(
 )
 
 reho_Schaefer_data.to_excel('Schaefer_data_alff.xlsx')
-
+'''
