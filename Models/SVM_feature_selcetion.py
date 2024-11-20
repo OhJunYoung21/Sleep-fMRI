5f0c4b81e954896accaa533a4b370bce3b63c386
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-11-20 15:02:30 +0900

    reho 이미지들의 평균값 차이 시각화 commit

--- Models/SVM_feature_selcetion.py
+++ Models/SVM_feature_selcetion.py
@@ -16,6 +16,9 @@ from Visualization.T_test import check_normality, student_t_test, welch_t_test, 
 
 schaefer_pkl = pd.read_pickle('../schaefer_data_pkl')
 
+different_nodes_alff = pd.DataFrame()
+different_nodes_alff['nodes'] = None
+
 
 def avoid_duplication(nested_list):
     unique_elements = set()
@@ -42,16 +45,18 @@ def statistic(rbd_data, hc_data):
     return np.unique(mann_test + student_test + welch_test).tolist()
 
 
+accuracy_score_mean = []
 feature_difference = []
-for i in range(20):
+
+for i in range(10):
     status_1_data = schaefer_pkl[schaefer_pkl['STATUS'] == 1]
     status_0_data = schaefer_pkl[schaefer_pkl['STATUS'] == 0]
     # Select only the REHO and STATUS columns
-    selected_data_1 = status_1_data[['REHO', 'STATUS']]
-    selected_data_0 = status_0_data[['REHO', 'STATUS']]
+    selected_data_1 = status_1_data[['fALFF', 'STATUS']]
+    selected_data_0 = status_0_data[['fALFF', 'STATUS']]
     # Split 80% of the data for training
-    train_data_1 = selected_data_1.sample(frac=0.7, random_state=42 + i)
-    train_data_0 = selected_data_0.sample(frac=0.7, random_state=42 + i)
+    train_data_1 = selected_data_1.sample(frac=0.8, random_state=42 + i)
+    train_data_0 = selected_data_0.sample(frac=0.8, random_state=42 + i)
 
     test_data_1 = selected_data_1.drop(train_data_1.index)
     test_data_0 = selected_data_0.drop(train_data_0.index)
@@ -59,21 +64,26 @@ for i in range(20):
     train_data = pd.concat([train_data_1, train_data_0])
     test_data = pd.concat([test_data_1, test_data_0])
 
-    rbd_data = train_data['REHO'][train_data['STATUS'] == 1]
-    hc_data = train_data['REHO'][train_data['STATUS'] == 0]
+    rbd_data = train_data['fALFF'][train_data['STATUS'] == 1]
+    hc_data = train_data['fALFF'][train_data['STATUS'] == 0]
 
-    result_list = statistic(rbd_data, hc_data)
+    '''
+    result = statistic(rbd_data, hc_data)
+    feature_difference.append(result)
+    
+    result = pd.read_pickle('different_nodes_alff.pkl')['nodes'].tolist()
 
-    feature_difference.append(result_list)
+    train_data['FC'] = train_data['ALFF'].apply(lambda x: [x[i] for i in result])
+    test_data['FC'] = test_data['ALFF'].apply(lambda x: [x[i] for i in result])
+    '''
 
-    result = avoid_duplication(feature_difference)
+    model = svm.SVC(kernel='rbf', C=1, probability=True)
+    model.fit(np.array(train_data['fALFF'].tolist()), train_data['STATUS'])
 
-    train_data['REHO'] = train_data['REHO'].apply(lambda x: [x[i] for i in result])
-    test_data['REHO'] = test_data['REHO'].apply(lambda x: [x[i] for i in result])
-
-    model = svm.SVC(kernel='linear', C=1)
-    model.fit(np.array(train_data['REHO'].tolist()), train_data['STATUS'])
-
-    accuracy = model.score(np.array(test_data['REHO'].tolist()), test_data['STATUS'])
+    accuracy = model.score(np.array(test_data['fALFF'].tolist()), test_data['STATUS'])
 
     print(f"{i + 1}th accuracy : {accuracy:.2f}")
+
+    accuracy_score_mean.append(accuracy)
+
+print(np.round(np.mean(accuracy_score_mean), 2))
--- Visualization/Visualization.py
+++ Visualization/Visualization.py
@@ -17,34 +17,27 @@ labels = AAL.labels
 AAL_atlas = input_data.NiftiLabelsMasker(labels_img=atlas_filename, standardize=True, strategy='mean',
                                          resampling_target="labels")
 
-alff_rbd_imgs = glob.glob(os.path.join('/Users/oj/Desktop/Yoo_Lab/CPAC_features/RBD/ReHo', 'ReHo_*.nii.gz'))
+reho_rbd_imgs = glob.glob(os.path.join('/Users/oj/Desktop/Yoo_Lab/CPAC_features/RBD/ReHo', 'ReHo_*.nii.gz'))
 reho_rbd = []
-alff_hc_imgs = glob.glob(os.path.join('/Users/oj/Desktop/Yoo_Lab/CPAC_features/HC/ReHo', 'ReHo_*.nii.gz'))
+reho_hc_imgs = glob.glob(os.path.join('/Users/oj/Desktop/Yoo_Lab/CPAC_features/HC/ReHo', 'ReHo_*.nii.gz'))
 reho_hc = []
 
-for k in alff_rbd_imgs:
+for k in reho_rbd_imgs:
     img = image.load_img(k)
-    aal_rbd = AAL_atlas.fit_transform(img)
-    reho_rbd.append(aal_rbd)
+    reho_rbd.append(img)
 
-for k in alff_hc_imgs:
+for k in reho_hc_imgs:
     img = image.load_img(k)
-    aal_hc = AAL_atlas.fit_transform(img)
-    reho_hc.append(aal_hc)
+    reho_hc.append(img)
 
-t_statistics = []
-p_values = []
+mean_rbd_img = image.mean_img(reho_rbd)
+mean_rbd_data = AAL_atlas.fit_transform(mean_rbd_img)
+mean_hc_img = image.mean_img(reho_hc)
+mean_hc_data = AAL_atlas.fit_transform(mean_hc_img)
 
-reho_rbd = np.array([item[0] for item in reho_rbd])
-reho_hc = np.array([item[0] for item in reho_hc])
-
-for i in range(116):
-    t_stat, p_val = ttest_ind(reho_rbd[:, i], reho_hc[:, i])
-    t_statistics.append(t_stat)
-    p_values.append(p_val)
-
-# 결과 출력
-for i in range(116):
-    print(f"Feature {i + 1}: T-statistic = {t_statistics[i]:.3f}, p-value = {p_values[i]:.3f}") if p_values[
-                                                                                                       i] <= 0.05 else print(
-        "None significant")
+mean_rbd_masked = AAL_atlas.inverse_transform(mean_rbd_data)
+mean_hc_masked = AAL_atlas.inverse_transform(mean_hc_data)
+plotting.plot_stat_map(mean_rbd_masked, title="RBD_mean", threshold=0.3)
+plotting.show()
+plotting.plot_stat_map(mean_hc_masked, title="HC_mean", threshold=0.3)
+plotting.show()
