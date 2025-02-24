81c13b6cfb22506e315d94b2f31e89d4121b3798
OhJunYoung21 <dhwnsdyd21@gmail.com>
2025-02-24 13:58:17 +0900

    정규성을 따르지 않는 데이터에 대해서는 전부 Mann-whitney U test 검정 commit

--- PET_classification/Statistic-analysis.py
+++ PET_classification/Statistic-analysis.py
@@ -6,7 +6,7 @@ from scipy.stats import brunnermunzel
 
 pet_data = pd.read_pickle('PET_shen_static.pkl')
 
-feature_name = 'FC'
+feature_name = 'REHO'
 
 ttest_data = pet_data[[feature_name, 'STATUS']]
 
@@ -27,39 +27,22 @@ for i in range(268):
     p_norm_1 = shapiro(positive_feature).pvalue
     p_norm_0 = shapiro(negative_feature).pvalue
 
-    ### PET_positive와 PET_negative사이에서 실제로 차이가 날 확률이 5%이상인 feature들에 대해서만 다음 작업을 수행한다.
-
-    if p_norm_1 > 0.05 and p_norm_0 > 0.05:
+    if p_norm_1 > 0.05 and p_norm_0 > 0.05:  # 두 그룹 모두 정규성을 만족하는 경우
+        # 등분산성 검정 (Levene’s test)
         p_levene = levene(positive_feature, negative_feature).pvalue
+        equal_var = p_levene > 0.05  # 등분산 여부 확인
 
-        if p_levene > 0.05:
-            t_stat, p_value = ttest_ind(positive_feature, negative_feature, equal_var=True)
-        else:
-            t_stat, p_value = ttest_ind(positive_feature, negative_feature, equal_var=False)
-    elif p_norm_0 < 0.05 and p_norm_1 < 0.05:
+        # t-test 수행
+        t_stat, p_value = ttest_ind(positive_feature, negative_feature, equal_var=equal_var)
+        test_used = "t-test (equal_var={})".format(equal_var)
 
-        p_levene = levene(positive_feature, negative_feature).pvalue
+    else:  # 하나라도 정규성을 따르지 않으면 비모수 검정 수행
+        u_stat, p_value = mannwhitneyu(positive_feature, negative_feature, alternative='two-sided')
+        test_used = "Mann-Whitney U test"
 
-        # Equally varianced
-
-        if p_levene > 0.05:
-            t_stat, p_value = mannwhitneyu(positive_feature, negative_feature, alternative='two-sided')
-
-        # Un-equally varianced
-        else:
-            t_stat, p_value = brunnermunzel(positive_feature, negative_feature)
-    else:
-        continue
-
-    ttest_result.append({
-        'Feature_Index': i,
-        'Shapiro_Positive_p': p_norm_1,
-        'Shapiro_Negative_p': p_norm_0,
-        'Levene_p': p_levene,
-        'T-Statistic': t_stat,
-        'P-Value': p_value
-    })
+    # 결과 저장
+    ttest_result.append({"Region": i, "Test": test_used, "p-value": p_value})
 
 t_test_result = pd.DataFrame(ttest_result)
 
-print(t_test_result)
+print(t_test_result[t_test_result['Test'] == "Mann-Whitney U test"])
