30d1a7ec310646a7d76253e4a014205da6ccbf5d
OhJunYoung21 <dhwnsdyd21@gmail.com>
2025-03-29 19:58:34 +0900

    Net_plot_brain Tutorial 예제(1)  : commit

--- Netplotbrain/main.py
+++ Netplotbrain/main.py
@@ -1,11 +1,16 @@
-import numpy as np
-import pandas as pd
-import nilearn
-from nilearn import image
-import nibabel as nib
 import netplotbrain
-from templateflow import api as tf
+import pandas as pd
+import numpy as np
+import templateflow.api as tf
+import itertools
+import matplotlib.pyplot as plt
 
-template = tf.get('MNI152NLin2009Asym', suffix="T1w")
+template = 'MNI152NLin2009cAsym'
+nodes_df = pd.DataFrame(data={'x': [40, 10, 30, -15, -25],
+                              'y': [50, 40, -10, -20, 20],
+                              'z': [20, 30, -10, -15, 30],
+                              'communities': [1, 1, 1, 2, 2],
+                              'degree_centrality': [1, 1, 0.2, 0.8, 0.4]})
 
-netplotbrain.plot(template=template)
+netplotbrain.plot(nodes=nodes_df, arrowaxis=None)
+plt.show()
